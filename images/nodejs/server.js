8f023637e85091c85cf072c5b9a3ddd1aeee15d0
Kusuma16 <kusumanetcom@gmail.com>
2023-08-18 09:57:29 +0800

    Add github CI/CD Pipeline

--- images/nodejs/server.js
+++ images/nodejs/server.js
@@ -9,6 +9,10 @@ app.get('/:name', (req, res) => {
     res.send(`Hello ${req.params.name}`);
 });
 
+app.get('/about', (req, res) => {
+    res.send('About page');
+});
+
 app.listen(3000, () => {
     console.log('Listening on port 3000');
 });
